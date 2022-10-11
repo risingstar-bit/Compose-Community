8971bcd00f2fed1533a314e468e5642ae4dac464
Syed Ashhar Rahman <92107937+ashhar-rahman@users.noreply.github.com>
2022-10-11 23:55:49 +0530

    Create Bubble_sort.c

---
+++ DSA/C/ALGORITHM/BUBBLE SORT/Bubble_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+    int i,n,a[100],item,temp,j;
+    printf("enter the order the element ");
+    scanf("%d", &n);
+    printf("enter the element ");
+    for (i=0;i<n;i++)
+    {
+      scanf("%d", &a[i]);
+    }
+     for(i=0;i<n;i++)
+     {
+         for(j=0;j<n-1;j++)
+         {
+             if(a[j]>a[j+1])
+              {
+              temp=a[j+1];
+              a[j+1]=a[j];
+              a[j]=temp;
+              }
+         }
+
+     }
+      for (i=0;i<n;i++)
+    {
+      printf("%d\t", a[i]);
+    }
+  
+}
