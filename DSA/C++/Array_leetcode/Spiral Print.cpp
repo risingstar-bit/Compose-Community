71b375be3ab02221cdd1b318da14b44c998a9cbb
Jainsuhani2021 <104892962+Jainsuhani2021@users.noreply.github.com>
2022-10-30 19:30:22 +0530

    Create Spiral Print.cpp

---
+++ DSA/C++/Array_leetcode/Spiral Print.cpp
@@ -0,0 +1,89 @@
+/*For a given two-dimensional integer array/list of size (N x M), print it in a spiral form. That is, you need to print in the order followed for every iteration:
+a. First row(left to right)
+b. Last column(top to bottom)
+c. Last row(right to left)
+d. First column(bottom to top)
+ Mind that every element will be printed only once.
+ input:
+1
+4 4 
+1 2 3 4 
+5 6 7 8 
+9 10 11 12 
+13 14 15 16
+output:
+1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 
+*/
+//Code:
+#include <iostream>
+using namespace std;
+void spiralPrint(int **input, int nRows, int nCols)
+{
+    
+    int i=0,j,w=0,rs=0,re=nRows-1,cs=0,ce=nCols-1;
+    while(w<nRows*nCols){
+      for (int i=cs;i<=ce and  w<nRows*nCols ;i++){
+          cout<<input[rs][i]<<" ";
+          w++;
+          
+      }
+        rs++;
+        for (int i=rs;i<=re and w<nRows*nCols  ;i++){
+          cout<<input[i][ce]<<" ";
+          w++;
+          
+      }
+        ce--;
+        
+        for (int i=ce;i>=cs and w<nRows*nCols  ;i--){
+          cout<<input[re][i]<<" ";
+          w++;
+          
+      }
+        re--;
+        
+        for (int i=re;i>=rs and  w<nRows*nCols;i--){
+          cout<<input[i][cs]<<" ";
+          w++;
+          
+      }
+        cs++;
+            
+            
+            
+        }
+        
+}
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int row, col;
+        cin >> row >> col;
+        int **matrix = new int *[row];
+
+        for (int i = 0; i < row; i++)
+        {
+            matrix[i] = new int[col];
+            for (int j = 0; j < col; j++)
+            {
+                cin >> matrix[i][j];
+            }
+        }
+        spiralPrint(matrix, row, col);
+
+        for (int i = 0; i < row; ++i)
+        {
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+
+        cout << endl;
+    }
+}
