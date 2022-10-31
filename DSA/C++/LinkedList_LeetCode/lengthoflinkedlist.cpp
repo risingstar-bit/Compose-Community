2a149f48cb089cc03002103cf04315daf3798625
garimapahwa <104934015+garimapahwa@users.noreply.github.com>
2022-10-31 22:34:34 +0530

    Create lengthoflinkedlist.cpp

---
+++ DSA/C++/LinkedList_LeetCode/lengthoflinkedlist.cpp
@@ -0,0 +1,59 @@
+//For a given singly linked list of integers, find and return its length. Do it using an iterative method.
+
+
+#include <iostream>
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;int length(Node *head)
+{
+    int count = 0;
+    Node* current = head;
+    while(current!=NULL) {
+        current = current-> next;
+        count++;
+    }
+    return count;
+}Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		cout << length(head) << endl;
+	}
+	return 0;
+}
