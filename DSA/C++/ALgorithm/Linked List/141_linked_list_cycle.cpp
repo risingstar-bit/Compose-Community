6f68e23b1f12f5cdfa0819776eebeb88bf598aff
parthmagicss <bigbangparth123@gmail.com>
2023-10-02 02:59:53 +0530

    adding linked list question

---
+++ DSA/C++/ALgorithm/Linked List/141_linked_list_cycle.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode * slow = head;
+        ListNode * fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
