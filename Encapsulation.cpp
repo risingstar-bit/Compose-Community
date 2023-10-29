83bdf460c545810dbb2078b6d013e1982d53e00a
Arbaaz Shaikh <shaikharbaz8456@gmail.com>
2023-10-29 22:58:01 +0530

    Create Encapsulation.cpp
    This is the code for accessing private class in C++ where Encapsulation method is used which also the part OOPs.

---
+++ Encapsulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+  private:
+    int salary;
+
+  public:
+    void setSalary(int s) {
+      salary = s;
+    }
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
+}
